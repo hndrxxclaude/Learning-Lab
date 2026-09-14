0efbcc17e9f604f444d90aea0b015d1162562e4c
GENTILE CLAUDIOMARIO <claudiomariogentile0@gmail.com>
2026-09-14 12:25:55 +0200

    Document Python type errors in python_type_errors.py

--- Python/30_Days_Of_Python/15_Day_Python_Type_Errors/python_type_errors.py
+++ Python/30_Days_Of_Python/15_Day_Python_Type_Errors/python_type_errors.py
@@ -1,1 +1,22 @@
+# print "Hello World" SyntaxError
 
+# print(age) NameError
+
+numbers = [1, 2, 3, 4, 5] 
+# print(numbers[5]) IndexError
+
+# import maths ModuleNotFoundError
+
+import math
+# math.PI AttributeError
+
+users = {'name': 'Claudio', 'age': 20, 'country': 'Italy'}
+# users['county'] KeyError
+
+# 4 + '3' TypeError
+
+# from math import power ImportError
+
+# int('12a') ValueError
+
+# 1 / 0 ZeroDivisionError
